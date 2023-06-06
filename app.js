e912a59216734a4c7a9cddb099abe9303fb69cb0
ZCP5 <zcp5@ZCP5s-MacBook-Pro.local>
2023-06-06 13:16:21 -0400

    use array of object color and food

--- app.js
+++ app.js
@@ -7,4 +7,23 @@ $(document).ready(() => {
     //appends the new div to starting element
     startingElement.after(siblingElement);
 
+const favoriteFood = $('#favorite-foods');
+console.log('favoriteFood')
+favoriteFood.css('background-color',' purple')
+
+const  favorite= [
+        {food: 'pizza', color: 'green'},
+        {food: 'falafel', color: 'yellow'},
+        {food: 'bolani', color: 'red'},
+        {food: 'hot dog', color: 'orange'}
+    ];
+    favorite.forEach(obj =>  {
+    let fovColors=$("<div></div>")
+    .text(obj.food)
+    fovColors.css('background-color',obj.color)
+    favoriteFood.after(fovColors);
+
+
+        })
+
 });
