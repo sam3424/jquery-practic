1b2b49e1d05857dea6fcc8e64da56f170a29e01b
ZCP5 <zcp5@ZCP5s-MacBook-Pro.local>
2023-06-06 11:04:15 -0400

    add div .define new div in jquery and add sibling

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 $(document).ready(() => {
     console.log('jquery is working');
+    const startingElement =$("#starting-point");
+    console.log(startingElement);
+    const siblingElement =$("<div></div>");
+    siblingElement.text("this is the next part");
+    //appends the new div to starting element
+    startingElement.after(siblingElement);
+
 });
